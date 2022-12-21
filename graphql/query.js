cb9c843c379b077a2e3a11c784b25c3dd83a94d9
Hlaing Htet <99160494+Hlaing-Htet@users.noreply.github.com>
2022-12-21 16:38:21 +0630

    day 1 init

---
+++ graphql/query.js
@@ -0,0 +1,46 @@
+export const GET_PRODUCTS = `
+query{
+  products{
+    data{
+      id
+      attributes{
+        title
+        slug
+        description
+        price
+        image{
+          data{
+            attributes{
+            formats
+             
+            }
+          }
+        }
+       
+      }
+    }
+  }
+}
+`;
+export const GET_PRODUCT = `
+query getProducts($slug: String!) {
+    products(filters: { slug: { eq: $slug } }) {
+      data {
+        id
+        attributes {
+          title
+          slug
+          description
+          price
+          image {
+            data {
+              attributes {
+                formats
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
